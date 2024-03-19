8664da9096f4b1544361a7e27d61805a72d1ffab
MuhamadFahriSalam <muhamadfahrisalam059@gmail.com>
2024-03-19 13:56:13 +0700

    fix

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# def print_star_pattern():
-#     for i in range(5):
-#         print(' ' * (4 - i) + '*' * (2 * i + 1))
-# print_star_pattern()
+def print_star_pattern():
+    for i in range(5):
+        print(' ' * (4 - i) + '*' * (2 * i + 1))
+print_star_pattern()
 
 
-# def print_word():
-#     for i in range(1, 6):
-#         if i == 4:
-#          print(f"{i}saya suka, pemrograman")
-#         else:
-#          print(f"{i}saya suka pemrograman")       
-# print_word()
+def print_word():
+    for i in range(1, 6):
+        if i == 4:
+         print(f"{i}saya suka, pemrograman")
+        else:
+         print(f"{i}saya suka pemrograman")       
+print_word()
 
 
 def print_h_pattern():
