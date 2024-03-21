ca71197bf5b58e911249b8e3ecbddf80e29110c7
MuhamadFahriSalam <muhamadfahrisalam059@gmail.com>
2024-03-21 16:58:05 +0700

    fix

---
+++ MuhamadFahriSalam.py
@@ -0,0 +1,25 @@
+def print_nilai():
+    # Obtain integer input from the user
+    number = int(input("Maukkan Bilangan: "))
+    # Determine if the number is even or odd
+    if number:
+        print("Adalah bilangan genap.")
+    else:
+        print("Adalah bilangan ganjil.")
+print_nilai()
+    
+    
+
+def print_loop():
+    # Initialize a counter variable
+    counter = 1
+    # Loop through the range from 1 to 11
+    for i in range(1, 11):
+        # Print the counter and the "yes!" flag
+        print(f"{counter} yes!")
+        # Increment the counter
+        counter += 1
+print_loop()
+    
+    
+    
