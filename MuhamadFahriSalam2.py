efe512aca87c2fb8cd2d22b896df55edf8b080ce
MuhamadFahriSalam <muhamadfahrisalam059@gmail.com>
2024-03-28 15:10:12 +0700

    fix

---
+++ MuhamadFahriSalam2.py
@@ -0,0 +1,20 @@
+merk = input('Masukkan merk: ')
+lengan = input('Masukkan panjang lengan: ')
+
+if merk.lower() == 'a':
+    if lengan.lower() == 'lengan pendek':
+        print('Harga: Rp 80.000')
+    elif lengan.lower() == 'lengan panjang':
+        print('Harga: Rp 87.000')
+    else:
+        print('Barang tidak ditemukan')
+        
+elif merk.lower() == 'b':
+    if lengan.lower() == 'lengan pendek':
+        print('Harga: Rp 75.000')
+    elif lengan.lower() == 'lengan panjang':
+        print('Harga: Rp 80.000')
+    else:
+        print('Barang tidak ditemukan')
+else:
+    print('Barang tidak ditemukan')
